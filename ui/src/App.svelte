985b907af443db68b052bc4b5c3af5e998feb025
Kris Hatcher <kris.hatcher@gmail.com>
2024-12-29 13:50:27 -0500

    feat: adding ui

---
+++ ui/src/App.svelte
@@ -0,0 +1,8 @@
+<main>
+  <h1>CampEquus</h1>
+  <h2>Track Humans and Horses Engaged in Training</h2>
+  <hr/>
+  <p>Summer camps often provide equestrian activities to their attendees. Interacting with horses is an incredible experience, it is <i>critical</i> that the camp staff know how to safely interact with these magnificent beasts in a way that educates attendees while maintaining a safe environment for both the horse and the attendee. This is where Equestrian Training providers come in, training camp staff in how to safely interact with horses, demonstrating curriculum for staff to use with attendees, and ensuring compliance with best practices.</p>
+  <p>We're working on developing an application that will allow Equestrian Training providers to track the humans and horses engaged in their training programs, issue certifications, and provide a dependable solution for data retention and collection.</p>
+  <p>Check back soon to get more details and join our launch!</p>
+</main>
